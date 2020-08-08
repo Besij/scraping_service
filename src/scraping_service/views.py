b20147e12868892c8a9ddd903e11bcc0cb9cbaf6
Besij <besij@mail.ru>
2020-08-08 12:51:53 +0700

    second commit

--- src/scraping_service/views.py
+++ src/scraping_service/views.py
@@ -1,9 +1,2 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-import datetime
 
-def home(request):
-    date = datetime.datetime.now().date()
-    name = 'Alex'
-    _context = {'date': date, 'name': name}
-    return render(request, 'home.html', context=_context)
